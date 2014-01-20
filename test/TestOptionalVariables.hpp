c674832994362194f3b33c4f6240267dbc694ab6
Jonathan Cooper <jonathan.cooper@cs.ox.ac.uk>
2014-01-20 13:41:11 +0000

    [FC] #2487 - allow defaults to be computed for optional model variables, tested with BR77 and INa_IV_curves.
    git-svn-id: file:///var/svn/chaste/projects/FunctionalCuration@20863 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- test/TestOptionalVariables.hpp
+++ test/TestOptionalVariables.hpp
@@ -53,8 +53,32 @@ class TestOptionalVariables : public CxxTest::TestSuite
 public:
     void TestClampingWithOldModel() throw (Exception)
     {
-        std::string dirname = "TestOptionalVariables";
+        std::string dirname = "TestOptionalVariables_TestClampingWithOldModel";
         ProtocolFileFinder proto_file("projects/FunctionalCuration/test/protocols/compact/test_INa_IV_curves.txt", RelativeTo::ChasteSourceRoot);
+        FileFinder cellml_file("projects/FunctionalCuration/cellml/beeler_reuter_model_1977.cellml", RelativeTo::ChasteSourceRoot);
+        ProtocolRunner runner(cellml_file, proto_file, dirname);
+        runner.RunProtocol();
+        FileFinder success_file(dirname + "/success", RelativeTo::ChasteTestOutput);
+        TS_ASSERT(success_file.Exists());
+
+        // Check the key outputs haven't changed.
+        const Environment& r_outputs = runner.GetProtocol()->rGetOutputsCollection();
+        NdArray<double> arr_to_check = GET_ARRAY(r_outputs.Lookup("normalised_peak_currents"));
+        TS_ASSERT_EQUALS(arr_to_check.GetNumElements(), 19u);
+        TS_ASSERT_EQUALS(arr_to_check.GetNumDimensions(), 1u);
+        const double expected[] = {1.0,0.999998,0.999981,0.999906,0.999564,0.999062,0.99795,0.995344,0.988466,0.966859,
+                                   0.887918,0.624599,0.192671,0.0186169,0.00193235,0.00116954,0.00114086,0.00113976,0.00113976};
+        NdArray<double>::ConstIterator it = arr_to_check.Begin();
+        for (unsigned i=0; i<19u; i++)
+        {
+            TS_ASSERT_DELTA(*it++, expected[i], 1e-3);
+        }
+    }
+
+    void TestDefaultExpressions() throw (Exception)
+    {
+        std::string dirname = "TestOptionalVariables_TestDefaultExpressions";
+        ProtocolFileFinder proto_file("projects/FunctionalCuration/test/protocols/compact/INa_IV_curves.txt", RelativeTo::ChasteSourceRoot);
         FileFinder cellml_file("projects/FunctionalCuration/cellml/beeler_reuter_model_1977.cellml", RelativeTo::ChasteSourceRoot);
         ProtocolRunner runner(cellml_file, proto_file, dirname);
         runner.RunProtocol();

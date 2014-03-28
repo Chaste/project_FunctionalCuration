a1d00c05751b7b7f7add7fd08263c6122bd6077a
Gary Mirams <gary.mirams@gmail.com>
2014-03-28 15:41:29 +0000

    [projects] Compatibility with #2410 and improve doxygen.
    git-svn-id: file:///var/svn/chaste/projects/FunctionalCuration@21348 b5f69653-2bf5-0310-8201-b0dafb8e1405

--- src/proto/ProtocolRunner.cpp
+++ src/proto/ProtocolRunner.cpp
@@ -154,7 +154,7 @@ boost::shared_ptr<AbstractSystemWithOutputs> ProtocolRunner::LoadModel(const Fil
     p_cell->SetMaxSteps(2e7); // We need to allow CVODE to take lots of internal steps for some protocols
     p_cell->SetTimestep(0.5); // Max dt = 0.5ms to ensure stimulus isn't missed
     p_cell->SetTolerances(/*rel*/1e-6, /*abs*/1e-8); // Guard against changes to defaults
-    p_cell->SetAutoReset(false); // Speedier simulations
+    // p_cell->SetForceReset(false); // This is now the default behaviour.
 
     ProtocolTimer::EndEvent(ProtocolTimer::LOAD_MODEL);
     return p_model;
